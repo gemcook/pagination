44960f1cd7c21ef6f221149bc23105dfd93b9f26
shsssskn <shsssskn@gmail.com>
2018-04-09 22:41:04 +0900

    [add] ### storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,23 @@
+/* @flow */
+import React from 'react';
+
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+import {withInfo} from '@storybook/addon-info';
+
+import {Welcome} from '@storybook/react/demo';
+
+import {Pagination} from '../src';
+
+import '../lib/base.css';
+import '../src/styles/index.scss';
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
+
+storiesOf('Pagination', module)
+  .addDecorator((story, context) => withInfo('common info')(story)(context))
+  .add('no data', () => <Pagination />)
+  .add('with props', () => <Pagination currentPage={1} total={10} />);
