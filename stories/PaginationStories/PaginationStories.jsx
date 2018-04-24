c96c457e4f4fd19f7b34249f04d7d6bcb8196c68
gctfuji <t.fujimoto@gemcook.com>
2018-04-24 12:05:17 +0900

    [fix] ### fix stories directory

---
+++ stories/PaginationStories/PaginationStories.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
+import {Pagination} from '../src';
+import {withState} from '@dump247/storybook-state';
+
+storiesOf('Pagination', module)
+  .addDecorator((story, context) => withInfo('common info')(story)(context))
+  .add(
+    'default',
+    withState({current: 1})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+        />
+      );
+    }),
+  )
+  .add(
+    'size of mini',
+    withState({current: 1})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+          size="mini"
+        />
+      );
+    }),
+  )
+  .add(
+    'size of small',
+    withState({current: 1})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+          size="small"
+        />
+      );
+    }),
+  )
+  .add(
+    'locale en_US',
+    withState({current: 1})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+          locale="en_US"
+        />
+      );
+    }),
+  )
+  .add(
+    'total is 0',
+    withState({current: 1})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={0}
+          changePage={current => {
+            store.set({current});
+          }}
+        />
+      );
+    }),
+  )
+  .add(
+    'change page size',
+    withState({current: 1, pageSize: 10})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+          showSizeChanger
+          onShowSizeChange={(current, pageSize) => {
+            store.set({pageSize});
+          }}
+          pageSize={store.state.pageSize}
+        />
+      );
+    }),
+  )
+  .add(
+    'pager (size options)',
+    withState({current: 1, pageSize: 15})(({store}) => {
+      return (
+        <Pagination
+          current={store.state.current}
+          total={1234}
+          changePage={current => {
+            store.set({current});
+          }}
+          showSizeChanger
+          pageSize={store.state.pageSize}
+          onShowSizeChange={(current, pageSize) => {
+            store.set({pageSize});
+          }}
+          pageSizeOptions={['15', '30', '100', '1000']}
+        />
+      );
+    }),
+  );
